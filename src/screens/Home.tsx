dbe020a86f7b11d68031115c17a657b8d140feeb
fellipeutaka <fellipeutaka@gmail.com>
2023-02-07 14:12:11 -0300

    fix: home favorites dependency useCallback

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -62,50 +62,55 @@ export function Home() {
     },
   });
 
-  const RecipeItem = useCallback(({ item }: ListRenderItemInfo<Result>) => {
-    return (
-      <Pressable
-        className="w-full h-64 relative mb-6"
-        onPressIn={() => {}}
-        onPress={() =>
-          navigate("Recipe", {
-            id: String(item.id),
-            title: item.title,
-            imageUrl: item.image,
-            favorites,
-            hasAlreadyFavorited: favorites.includes(String(item.id)),
-          })
-        }
-      >
-        <ImageBackground
-          className="flex-1 absolute inset-0 rounded-lg overflow-hidden"
-          source={{ uri: item.image }}
-          resizeMode="cover"
+  const RecipeItem = useCallback(
+    ({ item }: ListRenderItemInfo<Result>) => {
+      return (
+        <Pressable
+          className="w-full h-64 relative mb-6"
+          onPressIn={() => {}}
+          onPress={() =>
+            navigate("Recipe", {
+              id: String(item.id),
+              title: item.title,
+              imageUrl: item.image,
+              favorites,
+              hasAlreadyFavorited: favorites.includes(String(item.id)),
+            })
+          }
         >
-          <View className="flex-1 justify-between px-4 py-6 bg-black/60">
-            <View className="flex-row justify-between items-center">
-              <Text className="text-white font-medium">{item.sourceName}</Text>
-              <View className="flex-row items-center">
-                <ThumbsUp color="white" />
-                <Text className="text-white ml-1">{item.aggregateLikes}</Text>
-              </View>
-            </View>
-            <View>
-              <Text className="text-white text-xl font-semibold">
-                {item.title}
-              </Text>
-              <View className="flex-row items-center mt-4">
-                <Clock color="white" />
-                <Text className="text-white ml-1">
-                  {item.readyInMinutes} min
+          <ImageBackground
+            className="flex-1 absolute inset-0 rounded-lg overflow-hidden"
+            source={{ uri: item.image }}
+            resizeMode="cover"
+          >
+            <View className="flex-1 justify-between px-4 py-6 bg-black/60">
+              <View className="flex-row justify-between items-center">
+                <Text className="text-white font-medium">
+                  {item.sourceName}
                 </Text>
+                <View className="flex-row items-center">
+                  <ThumbsUp color="white" />
+                  <Text className="text-white ml-1">{item.aggregateLikes}</Text>
+                </View>
+              </View>
+              <View>
+                <Text className="text-white text-xl font-semibold">
+                  {item.title}
+                </Text>
+                <View className="flex-row items-center mt-4">
+                  <Clock color="white" />
+                  <Text className="text-white ml-1">
+                    {item.readyInMinutes} min
+                  </Text>
+                </View>
               </View>
             </View>
-          </View>
-        </ImageBackground>
-      </Pressable>
-    );
-  }, []);
+          </ImageBackground>
+        </Pressable>
+      );
+    },
+    [favorites]
+  );
 
   const keyExtractor = useCallback(({ id }: Result, index: number) => {
     return String(id + index);
